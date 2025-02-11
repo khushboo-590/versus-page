362c4519122e3a7f67cb8ca9258b173f3c4ed908
khushboo-590 <khushbook4979@gmail.com>
2025-02-11 11:23:16 +0530

    footer-section-responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,31 @@ menuIcon.addEventListener("click", () => {
     let activeNav = menuList.hasAttribute("class", "show");
     if (activeNav) { menuIcon.classList.toggle("active") };
 })
+
+
+$(document).ready(function () {
+    $('.my_file').slick({
+        dots: true,
+        infinite: true,
+        // speed: 250,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+
+    });
+});
